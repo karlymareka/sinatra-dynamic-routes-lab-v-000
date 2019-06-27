ba2362354a77a383555147e65236573194324022
Karly Mareka <karly.mareka@gmail.com>
2019-06-27 22:43:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ class App < Sinatra::Base
       when "add" 
         @solution = params[:number1].to_i + params[:number2].to_i
       when "subtract" 
-        @solution = params[:number1] - params[:number2]
+        @solution = params[:number1].to_i - params[:number2].to_i 
       when "multiply" 
         @solution = params[:number1] * params[:number2]
       when "divide" 
