0425f120e15a43f3732e2a169255123eaaa3353e
Karly Mareka <karly.mareka@gmail.com>
2019-06-27 22:39:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,6 +27,8 @@ class App < Sinatra::Base
     @five_words = params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5] + "." 
   end
   
-  
+  get '/:operation/:number1/:number2' do 
+    
+  end 
   
 end 
