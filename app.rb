0b04058efe9dc749070f432af076945ba2406cff
Karly Mareka <karly.mareka@gmail.com>
2019-06-27 22:44:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -38,7 +38,7 @@ class App < Sinatra::Base
       when "divide" 
         @solution = params[:number1].to_i / params[:number2].to_i 
       end 
-      @solution 
+      @solution.to_s  
   end 
   
 end 
